3f5bf72cc989116c607a60d23f760f51a9a018e1
cheikh <cheikhbeoumar@gmail.com>
2022-10-10 19:20:43 +0000

    Updates

---
+++ precache-manifest.c91bd41743cf7d37caf21a45e98fc3b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3e5c065cfa8b11d3026",
+    "url": "/github.io/React-temp1/static/css/main.c9677fa2.chunk.css"
+  },
+  {
+    "revision": "c3e5c065cfa8b11d3026",
+    "url": "/github.io/React-temp1/static/js/main.f305f70e.chunk.js"
+  },
+  {
+    "revision": "379693aecc5d1c0dd8f3",
+    "url": "/github.io/React-temp1/static/js/runtime~main.5ab68636.js"
+  },
+  {
+    "revision": "d409a52ea8bb7750e588",
+    "url": "/github.io/React-temp1/static/js/2.e7029b56.chunk.js"
+  },
+  {
+    "revision": "1972f29574012851c652",
+    "url": "/github.io/React-temp1/static/js/3.1749d0fc.chunk.js"
+  },
+  {
+    "revision": "7a002396155e8a5c99f19b7d8d59c1c1",
+    "url": "/github.io/React-temp1/index.html"
+  }
+];
